a89d518b4139832da054f29ecec34e2f47015dd0
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-17 09:40:35 +0530

    added: Deck of cards game setup

---
+++ DakeOfCards.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Sanobar Mujawar
+ * @since 17.06.21
+ * 
+ * Purpose: To perform deck of cards problem using oop concepts
+ *
+ */
+public class DakeOfCards {
+	
+	Cards card = new Cards();
+	
+	//main
+	public static void main(String[] args) {
+		
+		DakeOfCards deck = new DakeOfCards();
+		
+	}
+	
+}
