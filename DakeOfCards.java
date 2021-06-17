9e391b7183bad570cfa969e6b68f60166dfd980e
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-17 23:04:08 +0530

    added: method to shuffle 52 cards

--- DakeOfCards.java
+++ DakeOfCards.java
@@ -10,34 +10,51 @@
 
 package workshop;
 
+import java.util.Random;
+
 public class DakeOfCards {
-	
+
 	private static final int SIZE = 52;
-    private static Card[] deckOfCards = new Card[SIZE]; //created dake of cards of size 52
+	private static Card[] deckOfCards = new Card[SIZE]; // created dake of cards of size 52
 
-    /**
-     * UC-1 Method to return dake of cards with suits and ranks
-     * @return
-     */
-    public static Card[] getDeckOfCards() {
+	/**
+	 * UC-1 Method to return dake of cards with suits and ranks
+	 * 
+	 * @return
+	 */
+	public static Card[] getDeckOfCards() {
 
-        int count = 0;
+		int count = 0;
 
-        String[] suits = {"Diamonds", "Clubs", "Hearts", "Spades"};
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+		String[] suits = { "Diamonds", "Clubs", "Hearts", "Spades" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 
-        for (String s : suits) {
-            for (String r : ranks) {
+		for (String s : suits) {
+			for (String r : ranks) {
 
-                Card card = new Card(s, r);
-                deckOfCards[count] = card;
-                count++;
-            }
-        }
+				Card card = new Card(s, r);
+				deckOfCards[count] = card;
+				count++;
+			}
+		}
 
-        return deckOfCards;
+		return deckOfCards;
+	}
 
-    }    
+	/**
+	 * UC-4 Method to shuffle the 52 cards using random class
+	 * @param deckOfCards
+	 * @return
+	 */
+	public static Card[] shuffleCards(Card[] deckOfCards) {
+		Random rand = new Random();
+		int j;
+		for (int i = 0; i < SIZE; i++) {
+			j = rand.nextInt(SIZE);
+			Card temp = deckOfCards[i];
+			deckOfCards[i] = deckOfCards[j];
+			deckOfCards[j] = temp;
+		}
+		return deckOfCards;
+	}
 }
-	
-
