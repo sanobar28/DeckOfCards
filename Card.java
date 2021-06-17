51dc460b0037817899589e25860db2d38a2253fb
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-17 22:17:43 +0530

    added: Game setup for 52 cards for dake of card

---
+++ Card.java
@@ -0,0 +1,16 @@
+/**
+ * Class card stores string values of suit and rank
+ */
+package workshop;
+
+public class Card {
+
+	String suit;
+    String rank;
+
+   //constructor
+   public Card(String cardSuit, String cardRank){
+        this.suit = cardSuit;
+        this.rank = cardRank;
+    }
+}
--- DakeOfCards.java
+++ DakeOfCards.java
@@ -3,18 +3,41 @@
  * @author Sanobar Mujawar
  * @since 17.06.21
  * 
- * Purpose: To perform deck of cards problem using oop concepts
+ * Purpose: To perform deck of cards problem using oop and 
+ * Data structure concepts
  *
  */
+
+package workshop;
+
 public class DakeOfCards {
 	
-	Cards card = new Cards();
-	
-	//main
-	public static void main(String[] args) {
-		
-		DakeOfCards deck = new DakeOfCards();
-		
-	}
-	
+	private static final int SIZE = 52;
+    private static Card[] deckOfCards = new Card[SIZE]; //created dake of cards of size 52
+
+    /**
+     * UC-1 Method to return dake of cards with suits and ranks
+     * @return
+     */
+    public static Card[] getDeckOfCards() {
+
+        int count = 0;
+
+        String[] suits = {"Diamonds", "Clubs", "Hearts", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        for (String s : suits) {
+            for (String r : ranks) {
+
+                Card card = new Card(s, r);
+                deckOfCards[count] = card;
+                count++;
+            }
+        }
+
+        return deckOfCards;
+
+    }    
 }
+	
+
